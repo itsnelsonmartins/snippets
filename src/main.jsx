54a21369ab152d58352c995d3ccde6ff1fdee121
Nelson Martins <hi@itsnelsonmartins.com>
2023-04-05 21:44:34 +0200

    added easterEgg

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import './css/mediaQueries.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        {console.log(
+            '      ____     ____   ____   ____\r\n     /   /\\   /   /\\ /   /\\ /   /\\ \r\n    /   /_/__/   / //   / //   / / \r\n   /   _____    / //   / //___/ / \r\n  /   /\\___/   / //   / / \\___\\/ \r\n /___/ /  /___/ //___/ //___/\\ \r\n \\___\\/   \\___\\/ \\___\\/ \\___\\/\n\n'
+        )}
         <App />
     </React.StrictMode>
 );
