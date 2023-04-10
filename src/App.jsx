d817d4d2fbaaf19c5a33005e5766740e6f4e578c
Nelson Martins <hi@itsnelsonmartins.com>
2023-04-11 01:08:43 +0200

    quick fixes part 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import SnippetsGrid from './components/SnippetsGrid.component';
 
 import snippetsData from './snippets.json';
 
-import hc from '/hc.svg';
-
 const App = () => {
     const { search, theme } = useContext(ContextApi);
     const [searchValue, setSearchValue] = search;
@@ -19,41 +17,9 @@ const App = () => {
         .filter((tag, index, self) => self.indexOf(tag) === index)
         .sort();
 
-    // ThemeChanger function to change between dar and light mode
-    const themeChanger = () => {
-        // if (themeValue === 'light') {
-        //     document.firstElementChild.setAttribute('color-scheme', 'dark');
-        //     setThemeValue('dark');
-        // } else {
-        //     document.firstElementChild.setAttribute('color-scheme', 'light');
-        //     setThemeValue('light');
-        // }
-    };
-
     return (
         <div className='app'>
-            <nav
-                className='navbar'
-                role='navigation'
-                aria-label='main navigation'>
-                <div className='navbar-brand'>
-                    <div className='logo navbar-item'>
-                        <p className='title logo'>Snips!</p>
-                    </div>
-                </div>
-                <div className='navbar-item'>
-                    <Search />
-                </div>
-                <div className='navbar-end'>
-                    <div className='navbar-item'>
-                        <button className='button' onClick={themeChanger}>
-                            <span className='icon'>
-                                <img src={hc} alt='half circle symbol' />
-                            </span>
-                        </button>
-                    </div>
-                </div>
-            </nav>
+            <Search />
             <section className='section'>
                 <div className='columns'>
                     <div className='column is-2'>
--- src/components/search.component.jsx
+++ src/components/search.component.jsx
@@ -1,28 +1,53 @@
 import { useContext } from 'react';
 import { ContextApi } from '../ContextApi';
 
+import hc from '/hc.svg';
+
 const Search = () => {
     const { search } = useContext(ContextApi);
     const [searchValue, setSearchValue] = search;
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-    };
 
     const handleChange = (e) => {
         setSearchValue(e.target.value);
     };
 
+    // ThemeChanger function to change between dar and light mode
+    const themeChanger = () => {
+        // if (themeValue === 'light') {
+        //     document.firstElementChild.setAttribute('color-scheme', 'dark');
+        //     setThemeValue('dark');
+        // } else {
+        //     document.firstElementChild.setAttribute('color-scheme', 'light');
+        //     setThemeValue('light');
+        // }
+    };
+
     return (
-        <>
-            <input
-                className='input'
-                type='text'
-                value={searchValue}
-                placeholder='Filter...'
-                onChange={handleChange}
-            />
-        </>
+        <nav className='navbar' role='navigation' aria-label='main navigation'>
+            <div className='navbar-brand'>
+                <div className='logo navbar-item'>
+                    <p className='title logo'>Snips!</p>
+                </div>
+            </div>
+            <div className='navbar-item'>
+                <input
+                    className='input'
+                    type='text'
+                    value={searchValue}
+                    placeholder='Filter...'
+                    onChange={handleChange}
+                />
+            </div>
+            <div className='navbar-end'>
+                <div className='navbar-item'>
+                    <button className='button' onClick={themeChanger}>
+                        <span className='icon'>
+                            <img src={hc} alt='half circle symbol' />
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </nav>
     );
 };
 
